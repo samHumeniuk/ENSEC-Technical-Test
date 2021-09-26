60b957e6c610306f331626bd5875815d9ea3471b
Sam Humeniuk <sam_humeniuk@hotmail.com>
2021-09-26 16:06:39 +0100

    Fixing issue where hexidecimal numbers were accepted

--- MeterReadingAPI/MeterReadingAPI/MeterReadingCsvParser.cs
+++ MeterReadingAPI/MeterReadingAPI/MeterReadingCsvParser.cs
@@ -1,5 +1,6 @@
 using MeterReadingDataAccessLayer;
 using System;
+using System.Linq;
 
 namespace MeterReadingAPI
 {
@@ -11,7 +12,14 @@ namespace MeterReadingAPI
             var meterReading = new MeterReading();
             meterReading.AccountId = Convert.ToInt32(values[0]);
             meterReading.MeterReadingDateTime = Convert.ToDateTime(values[1]);
-            meterReading.MeterReadingValue = Convert.ToInt32(values[2]);
+
+            var meterReadingAsString = values[2];
+            if(!meterReadingAsString.All(char.IsDigit))
+            {
+                throw new FormatException($"The meter reading value of {meterReadingAsString} could not be converted to a number");
+            }
+            meterReading.MeterReadingValue = Convert.ToInt32(meterReadingAsString);
+
             return meterReading;
         }
     }
